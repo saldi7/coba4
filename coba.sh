c55ea641af4d62fba9e8bd0f835fc5cb700a4552
saldi7 <saldisalasing@gmail.com>
2021-04-02 00:25:04 +0800

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+chmod +x SRBMiner-MULTI
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj
